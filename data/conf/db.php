9e25f04974cd0a7dda3046d4cbbc921abbd42f87
LiTao <1287862489@qq.com>
2016-05-16 14:31:32 +0800

    first-allwin

---
+++ data/conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'allwinbio',
+    'DB_USER' => 'root',
+    'DB_PWD' => '123',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'cmf_',
+    //密钥
+    "AUTHCODE" => 'pA0x1rvUW34L2NtT7M',
+    //cookies
+    "COOKIE_PREFIX" => 'YjRQbI_',
+);
